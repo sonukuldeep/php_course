9c225c3044901f065c4a0b33515696ff24bb978a
sonukuldeep <sonukuldeep@outlook.com>
2023-12-17 22:43:54 +0530

    arithmatic

--- index.php
+++ index.php
@@ -1,30 +1,43 @@
 <?php
-$title = "Introduction to PHP";
-$author = "John Doe";
-$body = "PHP (Hypertext Preprocessor) is a widely used server-side scripting
-language that has revolutionized web development. With its simplicity,
-flexibility, and vast community support, PHP has become the backbone of
-countless dynamic websites and web applications.";
-$pageTitle = 'Brad\'s PHP Blog';
+$output = null;
+
+$num1 = 10;
+$num2 = 20;
+
+// Basic math
+$output = "$num1 + $num2 = " . $num1 + $num2;
+$output = "$num1 - $num2 = " . $num1 - $num2;
+$output = "$num1 * $num2 = " . $num1 * $num2;
+$output = "$num1 / $num2 = " . $num1 / $num2;
+$output = "$num1 % $num2 = " . $num1 % $num2;
+
+// Assignment operators
+$num1 += 10;
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?= $pageTitle . ' | ' . $title ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title><?php echo "Learn PHP From Scratch" ?></title>
 </head>
 
-<body>
-    <main>
-        <h1>Introduction to PHP</h1>
-        <p><?= 'By:' . ' ' . $author ?></p>
-        <p>
-            <?= $body ?>
-        </p>
-    </main>
+<body class="bg-gray-100">
+    <!-- this is header section -->
+    <header class="bg-blue-500 text-white p-4">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold"><?php echo "Learn PHP From Scratch" ?></h1>
+        </div>
+    </header>
+    <div class="container mx-auto p-4 mt-4">
+        <div class="bg-white rounded-lg shadow-md p-6">
+            <h2 class="text-2xl font-semibold mb-4"><?= "Welcome To The Course" ?></h2>
+            <p class="text-xl"><?= $num1 ?></p>
+        </div>
+    </div>
 </body>
 
 </html>
