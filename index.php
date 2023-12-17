dab3f561f6130a0cde05a1926037a8078ca6edcd
sonukuldeep <sonukuldeep@outlook.com>
2023-12-17 13:13:53 +0530

    variables

--- index.php
+++ index.php
@@ -1,31 +1,47 @@
 <?php
-$title = "Learn PHP From Scratch";
-$body = "In this course, you will learn the fundamentals of the PHP language";
-?>
 
-<!DOCTYPE html>
-<html lang="en">
+$name = "kuldeep";
+$lastName = "kumar";
+$age = 33;
+$rating = 4.5;
+$senior = true;
+$friends = ["Jonny", 20, 176.5];
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title><?= $title ?></title>
-</head>
+// string
+var_dump($name);
+echo "<br/>";
 
-<body class="bg-gray-100">
-    <!-- this is header section -->
-    <header class="bg-blue-500 text-white p-4">
-        <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold"><?php echo $title ?></h1>
-        </div>
-    </header>
-    <div class="container mx-auto p-4 mt-4">
-        <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4"><?= "Welcome To The Course" ?></h2>
-            <p><?= $body ?></p>
-        </div>
-    </div>
-</body>
+// string- just a different output format
+echo getType($lastName);
+echo "<br/>";
 
-</html>
+// integer
+var_dump($age);
+echo "<br/>";
+
+// float
+var_dump($rating);
+echo "<br/>";
+
+// boolean
+var_dump($senior);
+echo "<br/>";
+
+// array
+var_dump($friends);
+echo "<br/>";
+
+// Object
+$person = new stdClass();
+var_dump($person);
+echo "<br/>";
+
+// Null
+$car = null;
+var_dump($car);
+echo "<br/>";
+
+// Resourse
+$file = fopen('README.md', 'r');
+var_dump($file);
+echo "<br/>";
