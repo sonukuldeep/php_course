10c5d45ffe0f918d2445d485622278fb0844690c
sonukuldeep <sonukuldeep@outlook.com>
2024-01-11 10:02:09 +0530

    conditional in html

---
+++ chapter26_if_Statement.php
@@ -0,0 +1,12 @@
+<?php
+
+// control flow
+// if and if else
+$age = 21;
+if ($age >= 21) {
+    echo "You can legally get married now.";
+} else if ($age >= 18) {
+    echo "You are old enough to consume alcohol.";
+} else {
+    echo "You are too young to vote or get married.";
+}
